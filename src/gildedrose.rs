0751a332d8624937f642ded9bac177551630d236
yguenduez <yasinguenduez1@gmail.com>
2023-07-22 20:15:57 +0200

    add condition to computation, if sulfuras or not

--- src/gildedrose.rs
+++ src/gildedrose.rs
@@ -37,12 +37,17 @@ impl GildedRose {
             let quality = self.items[i].quality;
 
             let added_quality = self.calculate_quality_increment(&self.items[i], sell_in, quality);
-            let added_sell_in = -1;
+            let added_sell_in = if self.items[i].name.contains("Sulfuras") {
+                0
+            } else {
+                -1
+            };
 
             if !self.items[i].name.contains("Sulfuras") {
                 self.items[i].quality = (quality + added_quality).min(50).max(0);
-                self.items[i].sell_in = sell_in + added_sell_in;
             }
+
+            self.items[i].sell_in = sell_in + added_sell_in;
         }
     }
 
